2eef112355226413fb84c98eafb8b699e5608e9c
Your Name <you@example.com>
2022-10-16 00:10:38 +0530

    Added About page content

--- Frontend/src/Components/AboutSama.js
+++ Frontend/src/Components/AboutSama.js
@@ -14,8 +14,11 @@ function AboutSama(props) {
                     <p className="sidetext" >WHAT DOES SAMA DO?</p>
                 </div>
                 <div className='fill' style={{width: '95.45%', backgroundColor: '#404040'}}>
-                    <p className="header" style={{animation: 'fadeIn 1.05s', color: '#f2f2f2'}}>SAMA</p>
-                    <p className="stats" style={{animation: 'fadeIn 1.05s', color: '#f2f2f2'}}>25,10,500+</p>
+                    <p className="samaHeader" style={{animation: 'fadeIn 1.05s', color: '#f2f2f2'}}>WHAT DOES SAMA DO?</p>
+                    <p className="samaParagraph" style={{animation: 'fadeIn 1.05s', color: '#f2f2f2'}}>If you have a legal problem today, can you be sure of getting timely justice?<br/><br/>
+                        In a country where more than 4.5 crore cases are pending in the courts, timely justice can seem like a distant dream. Sama is a startup working towards alleviating this problem. <br/><br/>
+                        With Sama, you can resolve your legal problems while sitting at home, through the internet! Our motto is Suljhao, Magar Pyaar Se, which means 'to resolve with compassion'. We practice empathy, prize efficiency and deeply value the power of collaboration.<br/><br/>
+                        Another core value we hold dear is Transparency. By sharing with you live our case statistics, you can hold us accountable to be the best at resolving conflict, we know we will.</p>
                 </div> 
                 </>
                     )}
